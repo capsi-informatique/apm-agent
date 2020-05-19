8cec15e2da14e9ac5210b1ceda2c3299197c5130
Stephane Routhiau <stephane.routhiau@gmail.com>
2020-05-19 10:54:53 +0200

    fix2

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Usage wget https://raw.githubusercontent.com/capsi-informatique/apm-agent/master/provision.sh && chmod u+x provision.sh && sh provision.sh
+# Usage wget https://raw.githubusercontent.com/capsi-informatique/apm-agent/master/provision.sh && chmod u+x provision.sh && bash provision.sh
 
 AGENT_VERSION=1.15.0
 APM_DIR=/opt/apm
@@ -12,7 +12,7 @@ if [[ ! -e  $APM_DIR/elastic-apm-agent-$AGENT_VERSION.jar ]]; then
 	wget https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$AGENT_VERSION/elastic-apm-agent-$AGENT_VERSION.jar -P $APM_DIR
 fi
 
-wget -P https://raw.githubusercontent.com/capsi-informatique/apm-agent/master/elasticapm.properties -P $APM_DIR
+wget https://raw.githubusercontent.com/capsi-informatique/apm-agent/master/elasticapm.properties -P $APM_DIR
 
 echo Add following parameters to JVM option
 echo "-javaagent:$APM_DIR/elastic-apm-agent-$AGENT_VERSION.jar 	-Delastic.apm.environment=prod -Delastic.apm.service_name=myapp"
